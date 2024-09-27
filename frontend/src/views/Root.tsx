5b50a6eccbe4dc6e0312d41d3e2321237944ba1e
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2024-09-27 16:51:09 -0400

    images

--- frontend/src/views/Root.tsx
+++ frontend/src/views/Root.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Graph from "graphology";
 import { SigmaContainer, useLoadGraph } from "@react-sigma/core";
+import { NodeImageProgram } from "@sigma/node-image";
 import "@react-sigma/core/lib/react-sigma.min.css";
 
 import ShipOverview from "./ShipOverview";
@@ -29,13 +30,15 @@ export const LoadGraph = (props: {
             label: name,
             x: nodes[node][0],
             y: nodes[node][1],
-            size: 5,
+            size: 10,
+            color: "#00000000",
+            image: "http://localhost:3000/ship.png",
           });
         }
 
-        for (const edge of edges) {
-          graph.addEdge(edge.split("-")[0], edge.split("-")[1], { size: 0.1 });
-        }
+        // for (const edge of edges) {
+        //   graph.addEdge(edge.split("-")[0], edge.split("-")[1], { size: 0.1 });
+        // }
         props.setShips(ships);
       })
       .then(() => {
@@ -52,7 +55,26 @@ export default function Root() {
   return (
     <main>
       <ShipsContext.Provider value={ships}>
-        <SigmaContainer style={{ height: "100vh", width: "100vw" }}>
+        <SigmaContainer
+          style={{
+            height: "100vh",
+            width: "100vw",
+            // backgroundColor: "#10d5eb",
+            backgroundImage: "url(/ocean.webp)",
+            backgroundSize: "cover",
+          }}
+          settings={{
+            defaultNodeType: "image",
+            nodeProgramClasses: {
+              image: NodeImageProgram,
+            },
+            labelColor: {
+              color: "white",
+            },
+            labelWeight: "800",
+            labelDensity: 0.1,
+          }}
+        >
           <LoadGraph setShips={setShips} />
           <ShipOverview />
         </SigmaContainer>
