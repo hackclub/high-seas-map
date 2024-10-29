03e983ff8f0a01e3874ebd7958c01368ef68f892
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2024-10-28 22:47:51 -0400

    don't handle clicks on island

--- frontend/src/views/ShipOverview.tsx
+++ frontend/src/views/ShipOverview.tsx
@@ -49,7 +49,9 @@ export default function ShipOverview({
     const clickNodeListener = (payload: SigmaNodeEventPayload) => {
       const shipId = payload.node;
 
-      updateShip(shipId);
+      if (shipId !== "HIGH_SEAS_ISLAND") {
+        updateShip(shipId);
+      }
     };
 
     const clickOffListener = () => {
