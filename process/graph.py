de402b18034ab3a7d68dd2b1df38fe02b66acf30
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2024-12-02 20:52:55 -0500

    optimize graph generation

--- process/graph.py
+++ process/graph.py
@@ -1,166 +1,157 @@
-import json
 import igraph as ig
 import os
-from math import floor, sqrt
+from math import floor, sqrt, ceil
 import numpy as np
 import psycopg
-
-def process_graph():
-  good_graph_found = False
-  while (not good_graph_found):
-    good_graph_found = gen_graph()
+from joblib import Parallel, delayed
 
 def find_ship_name(ships, shipId):
   for ship in ships:
     if ship["id"] == shipId:
       return ship["name"]
 
-def gen_graph():
+def process_graph():
   with psycopg.connect(os.environ["DB_URI"]) as conn:
     with conn.cursor() as cur:
       cur.execute("SELECT (shipa, shipb, value) FROM similarity")
       data = cur.fetchall()
 
+    with conn.cursor() as cur:
+      cur.execute("SELECT id FROM ships WHERE filtered = true")
+      filtered_ships = cur.fetchall()
+
   if len(data) == 0:
     print("Similarities not processed.")
     return
 
-  edges = []
-  counted_ships = set()
-
   print("Building graph...")
-  g = ig.Graph()
-  for row in data:
+  def process_index(row):
     shipA = row[0][0]
     shipB = row[0][1]
     value = float(row[0][2])
 
-    # give each ship at least one edge
-    if ((shipA in counted_ships) and (shipB in counted_ships)) or value == 0:
-      if float(value) < 0.4:
-        continue
-
-    try:
-      g.vs.find(name=shipA)
-    except:
-      g.add_vertex(shipA)
-
-    try:
-      g.vs.find(name=shipB)
-    except:
-      g.add_vertex(shipB)
-
-    if shipA not in counted_ships:
-      counted_ships.add(shipA)
-    if shipB not in counted_ships:
-      counted_ships.add(shipB)
-    
-    g.add_edge(shipA, shipB, weight=float(value))
-    edges.append(f'{shipA}-{shipB}')
+    if float(value) < 0.5:
+      return None
+    else:
+      return ((shipA, shipB), float(value))
+  
+  edges_result = Parallel(n_jobs=4)(delayed(process_index)(row) for row in data)
+  filtered_ships = list(map(lambda row: row[0], filtered_ships))
+  g = ig.Graph()
+  g.add_vertices(filtered_ships)
+  edges, weights = list(zip(*filter(lambda r: r != None, edges_result)))
+  g.add_edges(edges, {
+    'weight': weights
+  })
 
   print("Plotting graph...")
-  
-  # np.random.seed(4)
-  seed = np.random.rand(len(g.vs), 2)
 
   mincoords = []
   maxcoords = []
 
   for v in g.vs:
     mincoords.append(0)
-    maxcoords.append(1)
-
-  layout = g.layout("kk", weights="weight", seed=seed, minx=mincoords, miny=mincoords, maxx=maxcoords, maxy=maxcoords)
-
-  nodes = {}
-  for i, coord in enumerate(layout._coords): 
-    key = g.vs[i]['name']
-
-    nodes[key] = coord
-
-  minX = 0
-  maxX = 0
-  minY = 0
-  maxY = 0
-  for key in nodes:
-    coords = nodes[key]
-    if coords[0] < minX:
-      minX = coords[0]
-    if coords[0] > maxX:
-      maxX = coords[0]
-    if coords[1] < minY:
-      minY = coords[1]
-    if coords[1] > maxY:
-      maxY = coords[1]
-
-  aspect = (maxX - minX) / (maxY - minY)
-
-  scaledNodes = {}
-  for nodeId in nodes.keys():
-    node = nodes[nodeId]
-
-    percentX = (node[0] - minX) / (maxX - minX)
-    scaledX = aspect * 200 * percentX
-
-    percentY = (node[1] - minY) / (maxY - minY)
-    scaledY = (1 / aspect) * 200 * percentY
-
-    scaledNodes[nodeId] = [scaledX, scaledY]
-
-  # island placing
-  print("Placing central island...")
+    maxcoords.append(10)
   
-  grid = []
-  for y in range(0, 200, 1):
-    row = []
+  # np.random.seed(4)
+  island_found = False
+  while not island_found:
+    seed = np.random.rand(len(g.vs), 2)
 
-    for x in range(0, 200, 1):
-      row.append(False)
+    layout = g.layout("kk", weights="weight", seed=seed, minx=mincoords, miny=mincoords, maxx=maxcoords, maxy=maxcoords)
 
-      for id in scaledNodes.keys():
-        n = scaledNodes[id]
+    nodes = {}
+    for i, coord in enumerate(layout._coords): 
+      key = g.vs[i]['name']
 
-        if floor(n[0]) == x and floor(n[1]) == y:
-          row[x] = True
-          break;
+      nodes[key] = coord
+
+    minX = 0
+    maxX = 0
+    minY = 0
+    maxY = 0
+    for key in nodes:
+      coords = nodes[key]
+      if coords[0] < minX:
+        minX = coords[0]
+      if coords[0] > maxX:
+        maxX = coords[0]
+      if coords[1] < minY:
+        minY = coords[1]
+      if coords[1] > maxY:
+        maxY = coords[1]
+
+    aspect = (maxX - minX) / (maxY - minY)
+
+    SCALE_RES = 400
+    scaledNodes = {}
+    for nodeId in nodes.keys():
+      node = nodes[nodeId]
+
+      percentX = (node[0] - minX) / (maxX - minX)
+      scaledX = aspect * SCALE_RES * percentX
+
+      percentY = (node[1] - minY) / (maxY - minY)
+      scaledY = (1 / aspect) * SCALE_RES * percentY
+
+      scaledNodes[nodeId] = [scaledX, scaledY]
+
+    # island placing
+    print("Placing central island...")
     
-    grid.append(row)
-  
-  islandW = 15
-  islandH = 15
+    grid = []
+    for y in range(0, SCALE_RES, 1):
+      row = []
 
-  xStreak = 0
-  islandLocations = []
-  for y in range(floor(len(grid) / 4), floor(3 * (len(grid) - islandH) / 4)):
-    for x in range(floor(len(grid[y]) / 4), floor(3 * (len(grid[y]) - islandW) / 4)):
-      if grid[y][x]:
-        xStreak += 1
+      for x in range(0, SCALE_RES, 1):
+        row.append(False)
+
+        for id in scaledNodes.keys():
+          n = scaledNodes[id]
+
+          if floor(n[0]) == x and floor(n[1]) == y:
+            row[x] = True
+            break;
       
-      if xStreak == islandW:
-        bad = False
+      grid.append(row)
+    
+    islandW = ceil(SCALE_RES * 0.01)
+    islandH = ceil(SCALE_RES * 0.01)
 
-        for checkY in range(y+1, y + islandH + 1):
-          for checkX in range(x, x + islandW + 1):
-            if grid[checkY][checkX]:
-              bad = True
-              xStreak = 0
+    xStreak = 0
+    islandLocations = []
+    for y in range(floor(len(grid) / 4), floor(3 * (len(grid) - islandH) / 4)):
+      for x in range(floor(len(grid[y]) / 4), floor(3 * (len(grid[y]) - islandW) / 4)):
+        if grid[y][x]:
+          xStreak += 1
+        
+        if xStreak == islandW:
+          bad = False
+
+          for checkY in range(y+1, y + islandH + 1):
+            for checkX in range(x, x + islandW + 1):
+              if grid[checkY][checkX]:
+                bad = True
+                xStreak = 0
+                break
+
+            if bad:
               break
+          
+          if not bad:
+            islandLocations.append([x - islandW + (islandW / 2), y + (islandH / 2)])
 
-          if bad:
-            break
+    if len(islandLocations) == 0:
+      print("No island location found for graph, regenerating...\n")
+      island_found = False
+    else:
+      island_found = True
         
-        if not bad:
-          islandLocations.append([x - islandW + (islandW / 2), y + (islandH / 2)])
-
-  if len(islandLocations) == 0:
-    print("No island location found for graph, regenerating...\n")
-    return False
-        
-  closestLocation = [100, 199]
-  closestDistance = sqrt(2 * (100 ** 2))
+  closestLocation = [SCALE_RES, SCALE_RES]
+  closestDistance = sqrt(2 * ((SCALE_RES / 2) ** 2))
 
   for location in islandLocations:
-    distance = sqrt(((location[0] - 100) ** 2) + ((location[1] - 100) ** 2))
+    distance = sqrt(((location[0] - (SCALE_RES / 2)) ** 2) + ((location[1] - (SCALE_RES / 2)) ** 2))
 
     if distance < closestDistance:
       closestDistance = distance
@@ -178,4 +169,4 @@ def gen_graph():
 
     conn.commit()
 
-  return True
+  print("Done with graph")
